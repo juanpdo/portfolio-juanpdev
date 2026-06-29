b1727413fb15a4228f40f364cd6985ee914b50e4
juanpdo <juanpdomail@gmail.com>
2026-06-29 21:31:12 +0200

    Implementación de INTERSECTIONOBSERVER, y ANIMACIONES en SECCIÓN PORTADA

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*-----------------------------APERTURA Y CIERRE MENÚ DE OPCIONES-------------------------------*/
+/*------------------------------------------------APERTURA Y CIERRE MENÚ DE OPCIONES----------------------------------------------*/
 
 let estadoMenu = "cerrado";
 
@@ -19,7 +19,7 @@ function cambiarEstadoMenu(){
 
 botonMenu.addEventListener("click",cambiarEstadoMenu);
 
-/*--------------------------------APERTURA Y CIERRE MODAL LEGAL----------------------------------*/
+/*-------------------------------------------------APERTURA Y CIERRE MODAL LEGAL-----------------------------------------------------*/
 
 const botonModal = document.getElementById("boton_legal");
 
@@ -37,3 +37,31 @@ function cerrarModalLegal(){
 
 botonModal.addEventListener("click",abrirModalLegal);
 botonCerraModal.addEventListener("click",cerrarModalLegal);
+
+/*---------------------------------------INTERSECTIONOBSERVER Y APARICIÓN DE ELEMENTOS------------------------------------------------*/
+
+function manejadorObservador(entradas,observador){
+
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add("visible");
+        }
+        else{
+           entrada.target.classList.remove("visible");
+        }
+    });
+};
+
+let opcionesObserador = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2
+}
+
+const observador = new IntersectionObserver(manejadorObservador,opcionesObserador);
+
+const secciones = document.querySelectorAll(".seccion");
+
+secciones.forEach(seccion => {
+    observador.observe(seccion);
+});
