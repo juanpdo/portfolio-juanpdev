cc6da26ebb80f361b39251b5ae48d2f46d15ba81
juanpdo <juanpdomail@gmail.com>
2026-06-29 16:04:24 +0200

    Implementación de APERTURA Y CIERRE DE MENÚ Y MODAL LEGAL; incluso modificación de <dialog> por <div> en MENÚ

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-document.querySelector(".modal-legal").showModal();
+/*-----------------------------APERTURA Y CIERRE MENÚ DE OPCIONES-------------------------------*/
+
+let estadoMenu = "cerrado";
+
+const botonMenu = document.getElementById("boton_settings");
+
+const ventanaMenu = document.getElementById("menu_configuracion");
+
+function cambiarEstadoMenu(){
+    if(estadoMenu == "cerrado"){
+        estadoMenu = "abierto";
+    }
+    else if(estadoMenu == "abierto"){
+        estadoMenu = "cerrado";
+    };
+
+    ventanaMenu.setAttribute("data-estado",estadoMenu);
+};
+
+botonMenu.addEventListener("click",cambiarEstadoMenu);
+
+/*--------------------------------APERTURA Y CIERRE MODAL LEGAL----------------------------------*/
+
+const botonModal = document.getElementById("boton_legal");
+
+const modalLegal = document.getElementById("legal_modal");
+
+const botonCerraModal = document.getElementById("boton_modal_cerrar");
+
+function abrirModalLegal(){
+    modalLegal.showModal();
+};
+
+function cerrarModalLegal(){
+    modalLegal.close();
+};
+
+botonModal.addEventListener("click",abrirModalLegal);
+botonCerraModal.addEventListener("click",cerrarModalLegal);
