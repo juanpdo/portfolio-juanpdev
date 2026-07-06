905f857cff1fb5e382986a5a7ae80e6164265f11
juanpdo <juanpdomail@gmail.com>
2026-07-06 23:56:04 +0200

    Otro intento más de arreglar el ENRUTADO en SPA NOTICIAS

--- functions/projects/spa-noticias/[[catchall]].js
+++ functions/projects/spa-noticias/[[catchall]].js
@@ -1,5 +1,10 @@
 export async function onRequest(context) {
     var url = new URL(context.request.url);
+    
+    if (url.pathname.includes('.') && !url.pathname.endsWith('.html')) {
+        return context.env.ASSETS.fetch(context.request);
+    }
+    
     url.pathname = '/projects/spa-noticias/index.html';
     return context.env.ASSETS.fetch(url);
 }
